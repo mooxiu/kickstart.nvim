506674a054e1f76d90d6c96a13877d06783d7e97
Muyao Xiao <monetshaw@outlook.com>
2025-05-06 16:53:45 +0900

    configure pretty

--- init.lua
+++ init.lua
@@ -474,7 +474,10 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'williamboman/mason.nvim', opts = {} },
+      {
+        'williamboman/mason.nvim',
+        opts = { ensure_installed = { 'prettier' } },
+      },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
